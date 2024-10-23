84b4b47b7230639dcf35c5bde868ec7dd4c3ed68
ltrwfs <ivanpanasiuk15@gmail.com>
2024-10-23 17:29:13 +0300

    Task 4 XML

---
+++ xml_reader.py
@@ -0,0 +1,27 @@
+import xml.dom.minidom as minidom
+
+DATASET_PATH = 'currency.xml'
+
+def get_objects(elements):
+    list = []
+    for node in elements:
+        for child in node.childNodes:
+            if child.nodeType == 1:
+                if child.tagName == 'Name':
+                    if child.firstChild.nodeType == 3:
+                        name = child.firstChild.data
+                if child.tagName == 'Nominal':
+                    if child.firstChild.nodeType == 3:
+                        nominal = float(child.firstChild.data)
+        if nominal == 1:
+            list.append(name)
+    return(list)
+
+if __name__ == '__main__':
+    with open(DATASET_PATH) as xml_file:
+        xml_data = xml_file.read()
+        dom = minidom.parseString(xml_data)
+        dom.normalize()
+        elements = dom.getElementsByTagName('Valute')
+        names_list = get_objects(elements)
+        print(', '.join(names_list))
